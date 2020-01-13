e1f3f5ef9bead5fc822eed54124664aa57409bab
shnhrrsn <shnhrrsn@gmail.com>
2020-01-12 22:17:59 -0500

    Added device config for GE 12724 3-Way Dimmer Switch

---
+++ src/Devices/0x0063/0x3031.ts
@@ -0,0 +1,8 @@
+import { IAccessoryConfig } from '../../IAccessoryConfig'
+
+// GE 12724 3-Way Dimmer Switch
+const deviceConfig: IAccessoryConfig = {
+	hints: ['light'],
+}
+
+export default deviceConfig
