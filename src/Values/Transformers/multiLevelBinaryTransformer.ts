400524f511d5cd9af45280f348cd04be425085d5
shnhrrsn <shnhrrsn@gmail.com>
2020-01-13 23:04:51 -0500

    Updated multiLevelBinaryTransformer to support truthy/falsey options

--- src/Values/Transformers/multiLevelBinaryTransformer.ts
+++ src/Values/Transformers/multiLevelBinaryTransformer.ts
@@ -1,13 +1,19 @@
 import { IValueTransformer } from './IValueTransformer'
 import { ValueType } from '../ValueType'
 
-export default function multiLevelBinaryTransformer(): IValueTransformer {
+export default function multiLevelBinaryTransformer(params?: {
+	truthy?: boolean | number
+	falsey?: boolean | number
+}): IValueTransformer {
+	const truthy = params?.truthy ?? true
+	const falsey = params?.falsey ?? false
+
 	return {
 		homekitToZwave(homekitValue: ValueType): ValueType {
 			return homekitValue ? 0xff : 0
 		},
 		zwaveToHomeKit(zwaveValue: ValueType): ValueType {
-			return zwaveValue > 0 ? true : false
+			return zwaveValue > 0 ? truthy : falsey
 		},
 	}
 }
