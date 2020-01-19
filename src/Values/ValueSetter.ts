6a472379c1e6d2a907b24e6be5a5390177868ae7
shnhrrsn <shnhrrsn@gmail.com>
2020-01-18 20:14:41 -0500

    Added ValueSetter class to handle concurrent sets

---
+++ src/Values/ValueSetter.ts
@@ -0,0 +1,78 @@
+import { ValueId } from 'openzwave-shared'
+import { IValueStreams } from '../Streams/IValueStreams'
+import { ValueType } from './ValueType'
+import takeFreshValue from '../Support/takeFreshValue'
+
+interface PendingSetValue {
+	value: ValueType
+	resolve(value: ValueType): void
+	reject(error: Error): void
+}
+
+/**
+ * Throttles concurrent sets to Zwave by ensuring on a single set is happening
+ * at any given time.  No guarantees are made with regards to whether or the
+ * the requested value to be set is actually set.  The most recent requested
+ * value will be set when it’s eligible.
+ *
+ * Consider the following example:
+ * 	1. `set` is called with a value of 87
+ *  2. `set` is called with a value of 63
+ *  3. `set` is called with a value of 8
+ *
+ * The first step will set 87 and block any future setting until it’s done.
+ * The second and third steps will both resolve to `8` since the third set is
+ * the most recent and the second step is queued alongside the third step.
+ */
+export default class ValueSetter {
+	valueId: ValueId
+	valueStreams: IValueStreams
+	private isSetting = false
+	private pendingSets: PendingSetValue[] = []
+
+	constructor(valueId: ValueId, valueStreams: IValueStreams) {
+		this.valueId = valueId
+		this.valueStreams = valueStreams
+	}
+
+	set(newValue: ValueType): Promise<ValueType> {
+		return new Promise((resolve, reject) => {
+			this.pendingSets.push({ resolve, reject, value: newValue })
+
+			if (this.isSetting) {
+				return
+			}
+
+			this.processNext()
+		})
+	}
+
+	private processNext() {
+		const sets = this.pendingSets
+		this.pendingSets = []
+
+		if (sets.length === 0) {
+			return
+		}
+
+		const { value: lastValue } = sets[sets.length - 1]
+		this.valueStreams.zwave.setValue(this.valueId, lastValue)
+		this.isSetting = true
+
+		takeFreshValue(this.valueStreams.valueChanged)
+			.then(() => {
+				for (const { resolve } of sets) {
+					resolve(lastValue)
+				}
+			})
+			.catch(error => {
+				for (const { reject } of sets) {
+					reject(error)
+				}
+			})
+			.finally(() => {
+				this.isSetting = false
+				this.processNext()
+			})
+	}
+}
---
+++ test/ValueSetter.test.ts
@@ -0,0 +1,42 @@
+import test from 'ava'
+import MockValue from '../mocks/MockValue'
+import ValueSetter from '../src/Values/ValueSetter'
+import makeZwaveSet from './helpers/makeZwaveSet'
+
+test('single set', async t => {
+	const zwave = makeZwaveSet(10)
+	const valueSetter = new ValueSetter(new MockValue(63), zwave)
+	t.is(87, await valueSetter.set(87))
+})
+
+test('double set', async t => {
+	const zwave = makeZwaveSet(10)
+	const valueSetter = new ValueSetter(new MockValue(63), zwave)
+	t.is(87, await valueSetter.set(87))
+	t.is(63, await valueSetter.set(63))
+})
+
+test('multi set', async t => {
+	const zwave = makeZwaveSet(50)
+	const valueSetter = new ValueSetter(new MockValue(63), zwave)
+
+	// This will trigger immediately
+	const set87 = valueSetter.set(87)
+
+	// The next 3 sets should be queued
+	// and the return value for all of them
+	// should match the last one
+	const set63 = valueSetter.set(63)
+	const set6 = valueSetter.set(6)
+	const set3 = valueSetter.set(3)
+
+	// Verify results of prior sets
+	t.is(87, await set87)
+	t.is(3, await set63)
+	t.is(3, await set6)
+	t.is(3, await set3)
+
+	// Just a simple test to make sure there’s no issues
+	// after the queue has been cleared
+	t.is(87, await valueSetter.set(87))
+})
