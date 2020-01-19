bfa1810ff6ffe7304e2e06aaca340e6ead03ccd2
shnhrrsn <shnhrrsn@gmail.com>
2020-01-19 17:34:01 -0500

    Updated shared zwave util to work with recent changes in src

--- utils/shared/zwave.ts
+++ utils/shared/zwave.ts
@@ -15,11 +15,19 @@ export interface Node {
 export default function zwave(): Promise<Map<number, Node>> {
 	const device = process.env.DEVICE_PATH
 
-	const zwave = new Zwave({
-		ConsoleOutput: false,
-		Logging: false,
-		SaveConfiguration: false,
-	})
+	const zwave = new Zwave(
+		{
+			debug: debug,
+			info: debug,
+			warn: debug,
+			error: debug,
+		},
+		{
+			ConsoleOutput: false,
+			Logging: false,
+			SaveConfiguration: false,
+		},
+	)
 
 	const nodes = new Map<number, Node>()
 
@@ -43,30 +51,34 @@ export default function zwave(): Promise<Map<number, Node>> {
 		node.info = nodeInfo
 	})
 
+	zwave.notification.subscribe(() => {
+		process.stdout.write(chalk.dim('.'))
+	})
+
 	zwave.valueAdded.subscribe(({ nodeId, value }) => {
 		process.stdout.write('.')
 		nodes.get(nodeId)?.values.push(value)
 	})
 
 	return new Promise((resolve, reject) => {
-		zwave.on('driver ready', () => {
+		zwave.ozw.on('driver ready', () => {
 			debug('Connected')
 		})
 
-		zwave.on('scan complete', () => {
+		zwave.ozw.on('scan complete', () => {
 			process.stdout.write('\n')
 			debug('Scan Complete')
-			setImmediate(() => zwave.disconnect(device))
+			setImmediate(() => zwave.ozw.disconnect(device))
 			resolve(nodes)
 		})
 
-		zwave.on('driver failed', () => {
+		zwave.ozw.on('driver failed', () => {
 			process.stdout.write('\n')
-			setImmediate(() => zwave.disconnect(device))
+			setImmediate(() => zwave.ozw.disconnect(device))
 			reject(new Error('Unable to connect to device.'))
 		})
 
 		debug(`Connecting to ${path.relative(process.cwd(), device)}`)
-		zwave.connect(device)
+		zwave.ozw.connect(device)
 	})
 }
