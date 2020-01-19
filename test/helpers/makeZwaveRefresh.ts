a70ce92550d682a5ae2ad23fefa9eae4136dea7d
shnhrrsn <shnhrrsn@gmail.com>
2020-01-18 21:38:53 -0500

    Added callback to makeZwaveRefresh

--- test/helpers/makeZwaveRefresh.ts
+++ test/helpers/makeZwaveRefresh.ts
@@ -6,12 +6,17 @@ import { ValueType } from '../../src/Values/ValueType'
 export default function makeZwaveRefresh(
 	refreshValue: ValueType,
 	setTimeoutInterval?: number,
+	callback?: () => void,
 ): IZwave {
 	const zwave = new MockZwave({
 		handleSetValue() {},
 		handleRefreshValue(valueId) {
 			const set = () => {
-				zwave.valueChanged.next({
+				if (callback) {
+					callback()
+				}
+
+				zwave.valueRefreshed.next({
 					nodeId: valueId.node_id,
 					comClass: valueId.class_id,
 					value: new MockValue(refreshValue),
