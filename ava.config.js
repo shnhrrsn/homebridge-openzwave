c8c53283f7c2f3643a0ecdc8406a542fa21a520a
shnhrrsn <shnhrrsn@gmail.com>
2020-01-18 20:03:37 -0500

    Added `makeZwaveSet` test helper

--- ava.config.js
+++ ava.config.js
@@ -3,5 +3,6 @@ export default function config() {
 		compileEnhancements: false,
 		extensions: ['ts'],
 		require: ['./utils/shared/bootstrap.js'],
+		files: ['test/**/*', '!test/helpers'],
 	}
 }
---
+++ test/helpers/makeZwaveSet.ts
@@ -0,0 +1,26 @@
+import { IZwave } from '../../src/Zwave/IZwave'
+import MockValue from '../../mocks/MockValue'
+import MockZwave from '../../mocks/MockZwave'
+
+export default function makeZwaveSet(setTimeoutInterval?: number): IZwave {
+	const zwave = new MockZwave({
+		handleSetValue(valueId, value) {
+			const set = () => {
+				zwave.valueChanged.next({
+					nodeId: valueId.node_id,
+					comClass: valueId.class_id,
+					value: new MockValue(value),
+				})
+			}
+
+			if (setTimeoutInterval) {
+				setTimeout(set, setTimeoutInterval)
+			} else {
+				set()
+			}
+		},
+		handleRefreshValue() {},
+	})
+
+	return zwave
+}
