ddbcf0ba669b9564ce2e2cdd10e24f00ca57fad5
shnhrrsn <shnhrrsn@gmail.com>
2020-01-27 22:45:55 -0500

    Fixed typing issues with exactlyOnce

--- src/Support/exactlyOnce.ts
+++ src/Support/exactlyOnce.ts
@@ -4,12 +4,28 @@ import { Homebridge } from '../../types/homebridge'
  * Wraps a callback and ensures it’s only called once.
  * Subsequent invocations are discarded.
  */
-export default function exactlyOnce<ParamType, ReturnType>(
-	callback: (param: ParamType, ...args: any) => ReturnType | undefined,
+
+export function exactlyOnce<A, R>(
+	callback: (a: A) => R | undefined,
 	log?: Homebridge.Logger,
-): (param: ParamType) => ReturnType | undefined {
+): (a: A) => R | undefined
+
+export function exactlyOnce<A, B, R>(
+	callback: (a: A, b: B) => R | undefined,
+	log?: Homebridge.Logger,
+): (a: A, b: B) => R | undefined
+
+export function exactlyOnce<A, B, C, R>(
+	callback: (a: A, b: B, c: C) => R | undefined,
+	log?: Homebridge.Logger,
+): (a: A, b: B, c: C) => R | undefined
+
+export default function exactlyOnce<R>(
+	callback: (...args: any) => R | undefined,
+	log?: Homebridge.Logger,
+): (...args: any) => R | undefined {
 	let hasFired = false
-	return function(param: ParamType, ...args: any): ReturnType | undefined {
+	return function(...args: any): R | undefined {
 		if (hasFired) {
 			if (log) {
 				log.warn('Attempted to call more than once', new Error().stack)
@@ -19,6 +35,6 @@ export default function exactlyOnce<ParamType, ReturnType>(
 			hasFired = true
 		}
 
-		return callback(param, ...args)
+		return callback(...args)
 	}
 }
